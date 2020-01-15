cc033247c15f1780aa12f697a8db1408c01b3ab9
Ruan <ruan4f@gmail.com>
2020-01-14 23:48:30 -0300

    Changes

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request,response) => {
-  return response.send('Hello World');
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' });
 });
 
 app.listen(3333);
